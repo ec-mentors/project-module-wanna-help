0a9c7eb9d2a2d036601c8c1614c1bf5abd3725a0
Hasan <e.hasan.yahya@gmail.com>
2023-05-30 13:26:37 +0200

    branches

---
+++ wanna-help/src/main/java/project/wanna_help/WannaHelpApplication.java
@@ -0,0 +1,13 @@
+package project.wanna_help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannaHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannaHelpApplication.class, args);
+	}
+
+}
---
+++ wanna-help/src/test/java/project/wanna_help/WannaHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package project.wanna_help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannaHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

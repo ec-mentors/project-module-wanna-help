57b76ee551a97b84c2b93b8b46097dfc2dc86ae6
catEvasion <catherine.schlienger@hotmail.com>
2023-06-07 08:37:46 +0200

    refactor: LoginEndpoint add: UserPrincipal

--- wanna-help/src/main/java/project/wanna_help/security/UserPrincipal.java
+++ wanna-help/src/main/java/project/wanna_help/security/UserPrincipal.java
@@ -50,4 +50,8 @@ public class UserPrincipal implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
 }

b249ba1bcc413085ccfc4fe4aa53695303db1e8e
Hasan <e.hasan.yahya@gmail.com>
2023-06-13 11:27:05 +0200

    rating update for done applications

--- wanna-help/src/main/java/project/wanna_help/profile/logic/RatingService.java
+++ wanna-help/src/main/java/project/wanna_help/profile/logic/RatingService.java
@@ -1,11 +1,14 @@
 package project.wanna_help.profile.logic;
 
-import org.hibernate.internal.HEMLogging;
 import org.springframework.stereotype.Service;
+import project.wanna_help.activity.persistence.domain.Application;
+import project.wanna_help.activity.persistence.domain.ApplicationStatus;
+import project.wanna_help.activity.persistence.repository.ApplicationRepository;
 import project.wanna_help.appuser.logic.UserHelper;
 import project.wanna_help.profile.communication.dto.RatingDTO;
 import project.wanna_help.profile.persistence.domain.HelpSeeker;
 import project.wanna_help.profile.persistence.domain.Rating;
+import project.wanna_help.profile.persistence.domain.Volunteer;
 import project.wanna_help.profile.persistence.repository.HelpSeekerRepository;
 import project.wanna_help.profile.persistence.repository.RatingRepository;
 
@@ -13,7 +16,6 @@ import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,13 +26,14 @@ public class RatingService {
     private final RatingConverter ratingConverter;
 
     private final HelpSeekerRepository helpSeekerRepository;
-
+    private final ApplicationRepository applicationRepository;
     private final UserHelper userHelper;
 
-    public RatingService(RatingRepository ratingRepository, RatingConverter ratingConverter, HelpSeekerRepository helpSeekerRepository, UserHelper userHelper) {
+    public RatingService(RatingRepository ratingRepository, RatingConverter ratingConverter, HelpSeekerRepository helpSeekerRepository, ApplicationRepository applicationRepository, UserHelper userHelper) {
         this.ratingRepository = ratingRepository;
         this.ratingConverter = ratingConverter;
         this.helpSeekerRepository = helpSeekerRepository;
+        this.applicationRepository = applicationRepository;
         this.userHelper = userHelper;
     }
 
@@ -38,10 +41,16 @@ public class RatingService {
         HelpSeeker helpSeeker = helpSeekerRepository.findById(helpSeekerId)
                 .orElseThrow(() -> new EntityNotFoundException("HelpSeeker not found with id: " + helpSeekerId));
 
-        if (ratingRepository.findOneByHelpSeekerAndVolunteer(helpSeeker, userHelper.getCurrentVolunteer()).isPresent()) {
-            throw new EntityExistsException("You can not give rating more than one time");
+        Volunteer currentVolunteer = userHelper.getCurrentVolunteer();
+        var listOfDoneApplications = applicationRepository.findByVolunteerAndApplicationStatus(currentVolunteer, ApplicationStatus.DONE);
 
-        } else {
+        boolean hasDoneApplication = false;
+
+        for (Application application : listOfDoneApplications) {
+            if (ratingRepository.findOneByHelpSeekerAndVolunteer(helpSeeker, userHelper.getCurrentVolunteer()).isPresent()) {
+                throw new EntityExistsException("You cannot give a rating more than once. You can update your rating after finishing another application with the same helpSeeker");
+            }
+
             Rating rating = new Rating();
             rating.setVolunteer(userHelper.getCurrentVolunteer());
             rating.setHelpSeeker(helpSeeker);
@@ -55,6 +64,12 @@ public class RatingService {
             ratingRepository.save(rating);
 
             helpSeekerRepository.save(helpSeeker);
+
+            hasDoneApplication = true;
+        }
+
+        if (!hasDoneApplication) {
+            throw new EntityNotFoundException("You cannot add a rating before your application is done");
         }
     }
 
